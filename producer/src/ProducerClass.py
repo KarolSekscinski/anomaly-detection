e1726742d3b678c573d01dbfe4fa2427fec67511
Karol Sekscinski <socer404@gmail.com>
2024-11-01 17:43:27 +0100

    Cleanup

--- producer/src/ProducerClass.py
+++ producer/src/ProducerClass.py
@@ -1,5 +1,6 @@
 from websocket_server import WebsocketServer
-from producer_functions import *
+
+from utils.producer_functions import *
 import logging
 from tqdm import tqdm
 import pandas as pd
@@ -13,22 +14,8 @@ class Producer:
                                                   self.kafka_config["PORT"])
 
     def run_server(self):
-        server = WebsocketServer(host=self.config['WEBSOCKET_SERVER']['host'],
-                                 port=self.config['WEBSOCKET_SERVER']['port'])
-        server.set_fn_new_client(self.new_client)
-        server.set_fn_message_received(self.message_received)
         logging.info("Server started")
-        server.run_forever()
-
-    def new_client(self, client, server):
-        logging.info(f"New client connected: {client['id']}")
-        server.send_message(client, "Welcome to the WebSocket Server!")
-
-    def message_received(self, client, server, message):
-        logging.info(f"Message received: {message}")
-        if message == "start_sending":
-            self.find_files_and_send_to_kafka()
-            server.send_message(client, "Data sent to Kafka!")
+        self.find_files_and_send_to_kafka()
 
     def find_files_and_send_to_kafka(self):
         files = find_files_matching_glob_pattern(self.config['DATASET_PATHS']['GOOGLE'])
@@ -40,7 +27,12 @@ class Producer:
         for _, row in df.iterrows():
             trade = row.to_dict()
             json_data = json.dumps(trade)
-            self.kafka_producer.send(topic_name, key=trade['symbol'], value=json_data)
+            self.kafka_producer.send(topic_name, value=json_data)
 
         self.kafka_producer.flush()
         print(f"All rows from {csv_file} have been sent to {topic_name}")
+
+
+if __name__ == "__main__":
+    producer = Producer()
+    producer.run_server()
